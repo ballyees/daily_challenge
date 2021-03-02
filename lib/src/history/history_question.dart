70fb65f8aee092cad263f933d89ac4a8e77314e7
ballyees <shouldertip@gmail.com>
2021-03-02 22:39:09 +0700

    add collapse item in day

--- lib/src/history/history_question.dart
+++ lib/src/history/history_question.dart
@@ -1,6 +1,7 @@
 import 'package:daily_challenge/src/api_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
 class HistoryQuestionPage extends StatefulWidget {
@@ -23,101 +24,97 @@ class _HistoryQuestionPageState extends State<HistoryQuestionPage> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             Map data = snapshot.data as Map;
-            print('data: ${snapshot.data}');
             List keys = data.keys.toList();
-            return
-              SingleChildScrollView(
-                child: Container(
-                  padding: EdgeInsets.all(8),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'HISTORY',
-                            style: TextStyle(fontSize: _textHeaderSize),
-                          )
-                        ],
-                      ),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            // Expanded(
-                            //   child: ExpansionTile(title: new Text("Numbers"),
-                            //     children: <Widget>[
-                            //       new Text("Number: 1"),
-                            //       new Text("Number: 2"),
-                            //       new Text("Number: 3"),
-                            //       new Text("Number: 4"),
-                            //       new Text("Number: 5")
-                            //     ],
-                            //   ),
-                            // ),
-                            Expanded(
-                              child: ListView.builder(
-                                itemCount: keys.length,
-                                shrinkWrap: true,
-                                itemBuilder: (context, index) => TimelineTile(
-                                  alignment: TimelineAlign.start,
-                                  indicatorStyle: IndicatorStyle(
-                                    color: Colors.amber,
-                                  ),
-                                  endChild: GestureDetector(
-                                    onTap: () {
-                                      print('1 Tap');
-                                    },
-                                    child: Container(
-                                      // color: Colors.lightGreenAccent,
-                                      constraints: const BoxConstraints(
-                                        minHeight: 50,
-                                      ),
-                                      child: Align(
-                                          alignment: Alignment.centerLeft,
-                                          child: Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 20),
-                                            child: ExpansionTile(
-                                              title: Text(
-                                                keys
-                                                    .elementAt(index)
-                                                    .toString(),
-                                                style: TextStyle(
-                                                    fontSize: _textBodySize),
-                                              ),
-                                              expandedAlignment:
-                                                  Alignment.centerLeft,
-                                              children: <Widget>[
-                                                ListView.builder(
-                                                  itemCount: data[
-                                                          keys.elementAt(index)]
-                                                      .length,
-                                                  itemBuilder:
-                                                      (context, innerIndex) =>
-                                                          Row(
+            return SingleChildScrollView(
+              physics: ScrollPhysics(),
+              child: Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'HISTORY',
+                          style: TextStyle(fontSize: _textHeaderSize),
+                        )
+                      ],
+                    ),
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                      Expanded(
+                        child: ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          itemCount: keys.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) => TimelineTile(
+                            isFirst: index == 0,
+                            isLast: index+1 == keys.length,
+                            alignment: TimelineAlign.start,
+                            indicatorStyle: IndicatorStyle(
+                              color: Colors.amber,
+                            ),
+                            endChild: GestureDetector(
+                              onTap: () {
+                                print('1 Tap');
+                              },
+                              child: Container(
+                                // color: Colors.lightGreenAccent,
+                                constraints: const BoxConstraints(
+                                  minHeight: 50,
+                                ),
+                                child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 20),
+                                      child: ExpansionTile(
+                                        title: Text(
+                                          keys.elementAt(index).toString(),
+                                          style: TextStyle(
+                                              fontSize: _textBodySize),
+                                        ),
+                                        expandedAlignment: Alignment.centerLeft,
+                                        children: [
+                                          ...List.generate(
+                                            data[keys.elementAt(index)].length,
+                                            (innerIndex) => Padding(
+                                              padding: const EdgeInsets.symmetric(vertical: 2),
+                                              child: Column(
                                                     children: [
-                                                      Column(
+                                                      Row(
                                                         children: [
-                                                          Text(data[keys.elementAt(index)][innerIndex]['create_date'].toString())
+                                                          Tooltip(
+                                                            message: data[keys.elementAt(index)][innerIndex]['create_date'].toString().substring(11),
+                                                            child: Chip(
+                                                              label: Text(data[keys.elementAt(index)][innerIndex]['create_date']
+                                                                  .toString().substring(11, 19)),
+                                                            ),
+                                                          ),
+                                                          SizedBox(width: 10,),
+                                                          Expanded(
+                                                            child: Text(data[keys.elementAt(index)][innerIndex]['question']
+                                                                .toString()),
+                                                          ),
                                                         ],
                                                       )
                                                     ],
                                                   ),
-                                                )
-                                              ],
-                                            ),
-                                          )),
-                                    ),
-                                  ),
-                                ),
+                                            )),
+                                          SizedBox(height: 10,)
+                                        ],
+                                      ),
+                                    )),
                               ),
-                            )
-                          ]),
-                    ],
-                  ),
+                            ),
+                          ),
+                        ),
+                      )
+                    ]),
+                  ],
                 ),
-              );
+              ),
+            );
           }
           return Center(child: CircularProgressIndicator());
         },
